daf22bdc77d03770a24e316ab69e68899e6790ea
Nguson <89438780+Nguson@users.noreply.github.com>
2021-08-24 00:19:52 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,10 @@
+public class Hello {
+	public static void main(String[]args) {
+		System.out.println("your name");
+		System.out.println("java program name");
+		System.out.println("program/project description");
+		System.out.println("date created");
+		System.out.println("course number and section number");
+		System.out.println();
+	}
+}
